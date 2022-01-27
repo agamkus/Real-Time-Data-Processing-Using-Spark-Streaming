fedf8f6c1b82f32114ff1f75a330072924b4e314
Agam Kushwaha <56078504+agamkus@users.noreply.github.com>
2022-01-28 00:52:02 +0530

    Create triggers.py

---
+++ Codes/Triggers And Output/triggers.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession  \
+	.builder  \
+	.appName("StructuredSocketRead")  \
+	.getOrCreate()
+
+spark.sparkContext.setLogLevel('WARN')
+	
+lines = spark  \
+	.readStream  \
+	.format("socket")  \
+	.option("host","localhost")  \
+	.option("port",12345)  \
+	.load()
+	
+query = lines  \
+	.writeStream  \
+	.outputMode("update")  \
+	.format("console")  \
+	.trigger(processingTime = '2 seconds')  \
+	.start()
+	
+query.awaitTermination()
